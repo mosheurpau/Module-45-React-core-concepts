1d8bc4952728b30819bab2d129c61b7e0292f989
mosheurpau <mosheur.pau@gamil.com>
2022-03-16 18:05:17 +0600

    react core concepts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 555;
+const singers = [
+  { name: 'Dr. Mahfuuz', job: 'Singer' },
+  { name: 'Eva Rahman', job: 'Singer2' },
+  { name: 'Agun', job: 'sopno' },
+  { name: 'shuvro', job: 'pathir' }];
+
+const singerStyle = {
+  color: 'green',
+  backgroundColor: 'yellow'
+}
+
 function App() {
+  const nayoks = ['Ruble', 'BappaDa', 'Kuber', 'Jashim', 'Salman Shah', 'Riyaz', 'Anuar'];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        nayoks.map(nayok => <li>Name: {nayok}</li>)
+      }
+      {/* {
+        nayoks.map(nayok => <Preson name={nayok}></Preson>)
+      } */}
+
+      {
+        singers.map(singer => <Preson name={singer.name}></Preson>)
+      }
+
+      {/* <Preson name="Rubel" nayika="moushomi"></Preson>
+      <Preson name="BappaRaz" nayika="seka"></Preson>
+      <Preson name="Kuber" nayika="kopila"></Preson> */}
+      <h5>New component. YAY</h5>
+      <p>rock mama React mama.</p>
+      <Friend movie="Shingam" phone="01754675993"></Friend>
+      <Friend phone="01969158499"></Friend>
     </div>
   );
+}
+
+function Preson(props) {
+  console.log(props);
+  return (
+    <div className='person'>
+      <h1>{props.name}</h1>
+      <p>{props.nayika}</p>
+    </div>
+  )
+}
+
+function Friend(props) {
+  console.log(props);
+  return (
+    <div className='container'>
+      <h3>Movie: {props.movie}</h3>
+      <p>Phone: {props.phone}</p>
+    </div>
+  )
 }
 
 export default App;
